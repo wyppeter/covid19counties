5e333793a741f02e6d157ee79d27e00d6cb27e83
Peter Y. Wang <15715249+wyppeter@users.noreply.github.com>
2020-04-17 16:57:32 -0400

    v2 update + 04/17
    Some automated parameter sensing, easier T/F switches for modes instead of commenting lines, log axis for growth rate with benchmark lines, and datestamp in title.

--- covid19.R
+++ covid19.R
@@ -11,25 +11,27 @@ library(gridExtra)
 library(lubridate)
 
 # Get county-level data; could take some seconds
-# df.raw = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+df.raw = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 
 # Choose state
 # Recommended parameter values
-STATE = "Massachusetts"  ;c=7;k=2;d=2
-# STATE = "New York"       ;c=4;k=1;d=4
-# STATE = "New Jersey"     ;c=5;k=3;d=3
-# STATE = "Washington"     ;c=3;k=1;d=8
-# STATE = "Florida"        ;c=5;k=1;d=4
-# STATE = "Michigan"       ;c=5;k=1;d=4
-# STATE = "California"     ;c=5;k=1;d=5
-# STATE = "Louisiana"      ;c=5;k=2;d=6
-# STATE = "Pennsylvania"   ;c=5;k=2;d=4
-# STATE = "Connecticut"    ;c=3;k=1;d=3
+STATE = "Massachusetts"  ;c=7;k=2
+# STATE = "New York"       ;c=4;k=1
+# STATE = "New Jersey"     ;c=5;k=3
+# STATE = "Washington"     ;c=3;k=1
+# STATE = "Florida"        ;c=5;k=1
+# STATE = "Michigan"       ;c=5;k=1
+# STATE = "California"     ;c=5;k=1
+# STATE = "Louisiana"      ;c=5;k=2
+# STATE = "Pennsylvania"   ;c=5;k=2
+# STATE = "Connecticut"    ;c=3;k=1
 
 # Parameters
+SAVEPLOTS = T  # save or view plots at the end
+SMOOTH = T  # 3-day smoothing of plot or raw trends
 COLNUM = c  # number of counties to show color and label for
-k = k  # divide total count by k to make graph easier to see
-DOUBLINGWKS = d  # 2/3/4/5/6/etc. weeks as upper limit of doubling time graphs
+k = k  # divide state total count by k to make graph easier to see
+
 
 # Filters for N when calculating rates
 CASEFILTER = 50  # rates calculated only when cases exceeed
@@ -37,11 +39,13 @@ DEATHFILTER = 10  # rates calculated only when deaths exceeed
 
 # MA policies (1. stay-at-home, 2. face masks) with delay-day delay for cases; use geom_blank() for other states
 delay = 10
-# MApolicy1 = geom_vline(xintercept = ymd(20200324)+delay, linetype = "dashed", color = "salmon", alpha = 0.5, size = 0.8)
-# MApolicy2 = geom_vline(xintercept = ymd(20200410)+delay, linetype = "dashed", color = "cadetblue", alpha = 0.5, size = 0.8)
-MApolicy1 = geom_blank()
-MApolicy2 = geom_blank()
-
+if (STATE == "Massachusetts"){
+  MApolicy1 = geom_vline(xintercept = ymd(20200324)+delay, linetype = "dashed", color = "salmon", alpha = 0.5, size = 0.8)
+  MApolicy2 = geom_vline(xintercept = ymd(20200410)+delay, linetype = "dashed", color = "cadetblue", alpha = 0.5, size = 0.8)
+}else{
+  MApolicy1 = geom_blank()
+  MApolicy2 = geom_blank()
+}
 
 # Set ggplot2 theme
 theme1 = theme(
@@ -87,6 +91,7 @@ maxDate = max(ymd(df.raw$date))
 x.ticks.halfm = scale_x_date(breaks = c(ymd("20200301","20200315","20200401","20200415","20200501","20200515","20200601","20200615")),
                              date_labels = "%b %d",
                              limits = c(startDate,maxDate))
+if(SMOOTH){date.axis.smooth = "Date (3-day average)"}else{date.axis.smooth = "Date"}
 
 # Filter and process data
 df = df.raw %>%
@@ -112,18 +117,72 @@ df.STATE.sum = df %>%  # we need state-total reference as well
   ungroup()
 
 df = df %>%
+  filter(county != "Unknown") %>%
   select(date, cases, deaths, county) %>%
   rbind(df.STATE.sum, .) %>%  # putting everything together
   mutate(county = factor(county, c(unique(county),"Others"))) %>%  # add Others to factor for color legend display
   mutate(county.o = factor(county, unique(arrange(df,desc(county))$county)))  # use this to order the graphs so the higher ones cover the lower ones, not the other way round
 
+df.trends = df %>%
+  # Calculate deltas
+  mutate(
+    cases.growth = abs(cases - lag(cases)),  # abs is a hack to prevent -ve number math error in log for doubling time; will be cleaned up by date filter
+    deaths.growth = abs(deaths - lag(deaths)),
+    cases.growth.rate = cases.growth/lag(cases),
+    deaths.growth.rate = deaths.growth/lag(deaths)
+  ) %>%
+  # Smoothing
+  mutate(
+    cases.growth.smooth = zoo::rollapply(
+      cases.growth, mean, width = 3, by = 1, align = "center", fill = NA  # rolling window averaging
+    ),
+    deaths.growth.smooth = zoo::rollapply(
+      deaths.growth, mean, width = 3, by = 1, align = "center", fill = NA
+    ),
+    cases.growth.rate.smooth = zoo::rollapply(
+      cases.growth.rate, mean, width = 3, by = 1, align = "center", fill = NA
+    ),
+    deaths.growth.rate.smooth = zoo::rollapply(
+      deaths.growth.rate, mean, width = 3, by = 1, align = "center", fill = NA
+    )) %>%
+  # Calculate doubling times
+  mutate(
+    doublingtime = log(2)/log(1+if(SMOOTH){cases.growth.rate.smooth}else{cases.growth.rate}),
+    ddoublingtime = log(2)/log(1+if(SMOOTH){deaths.growth.rate.smooth}else{deaths.growth.rate})
+  ) %>%
+  # The first and last of 3-day averaging is not relevant; +2 gets rid of the first one that is a delta from the max of previous county in the list
+  group_by(county) %>%
+    filter(
+      if(SMOOTH){
+        date >= min(date)+2 & date <= max(date)-1
+      }else{
+        date >= min(date)+1 & date <= max(date)
+      }
+    ) %>%
+  ungroup()
+
 # We are only id-ing and giving colors to the top several counties
 countiesToShow = c(as.character(unique(filter(df, as.numeric(county) < 2+COLNUM)$county)), "Others")
 colorscale = scale_colour_manual(values = genCol(coln = COLNUM), breaks = countiesToShow, drop = FALSE)
 
-# This the key geom we are plotting every time
+# Plot components to set up
+# General lin plot geom
 geom_line_plot  = geom_line(size = 0.5, alpha = 0.7, aes(group = county.o))
-
+# Growth rate y-axis
+y.lim.rate = coord_cartesian(ylim = c(0.01,0.75))
+y.ticks.pc = scale_y_log10(labels = scales::percent,
+                           breaks = c(0.01,0.02,0.03,0.05,0.10,0.20,0.30,0.50,0.75),
+                           minor_breaks = NULL)
+# Doubling time y-axis
+DOUBLINGWKS = ((unname(max(
+  filter(
+    df.trends,
+    cases >= CASEFILTER,
+    county == paste0("TOTAL/", k)
+  )$doublingtime
+))*1.2) %/% 7) + 1
+y.lim.wks = coord_cartesian(ylim = c(0, 7*DOUBLINGWKS))
+y.ticks.weeks = scale_y_continuous(breaks = seq(0,7*DOUBLINGWKS,7))
 
 ########################################
 # We start graphing here:
@@ -132,7 +191,6 @@ geom_line_plot  = geom_line(size = 0.5, alpha = 0.7, aes(group = county.o))
 
 # Graph #1: case trends
 G1 = df %>%
-  filter(county != "Unknown") %>%
   ggplot(aes(x = date, y = cases, color = county)) +
   geom_line_plot +
   scale_y_log10(limits = c(1,NA)) +
@@ -144,22 +202,12 @@ G1 = df %>%
   theme1
 
 # Graph #2: daily case counts, with 3-day smoothing
-df.growth = df %>%
-  mutate(cases.growth = cases - lag(cases)) %>%
-  filter(!is.na(cases.growth) & cases.growth >= 0) %>%
-  mutate(cases.growth.smooth = zoo::rollapply(
-    cases.growth, mean, width = 3, by = 1, align = "center", fill = NA
-  )) %>%
-  group_by(county) %>%
-  filter(date > min(date) & date < max(date)) %>%
-  ungroup()
-G2 = df.growth %>%
-  filter(county != "Unknown") %>%
-  ggplot(aes(x = date, y = cases.growth.smooth, color = county)) +
+G2 = df.trends %>%
+  ggplot(aes(x = date, y = if(SMOOTH){cases.growth.smooth}else{cases.growth}, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
   ggtitle("Daily case growth") +
-  xlab("Date (3-day average)") +
+  xlab(date.axis.smooth) +
   MApolicy1 + MApolicy2 +
   colorscale +
   theme1
@@ -171,34 +219,22 @@ G2 = df.growth %>%
 
 # Graph #3: death trends
 G3 = df %>%
-  filter(county != "Unknown") %>%
   ggplot(aes(x = date, y = deaths, color = county)) +
   geom_line_plot +
   scale_y_log10(limits = c(1,NA)) +
   x.ticks.halfm +
   ggtitle("Confirmed deaths (log10)") +
   xlab("Date") +
-
   colorscale +
   theme1
 
 # Graph #4: daily case counts, with 3-day smoothing
-df.dgrowth = df %>%
-  mutate(deaths.growth = deaths - lag(deaths)) %>%
-  filter(!is.na(deaths.growth) & deaths.growth >= 0) %>%
-  mutate(deaths.growth.smooth = zoo::rollapply(
-    deaths.growth, mean, width = 3, by = 1, align = "center", fill = NA
-  )) %>%
-  group_by(county) %>%
-  filter(date > min(date) & date < max(date)) %>%
-  ungroup()
-G4 = df.dgrowth %>%
-  filter(county != "Unknown") %>%
-  ggplot(aes(x = date, y = deaths.growth.smooth, color = county)) +
+G4 = df.trends %>%
+  ggplot(aes(x = date, y = if(SMOOTH){deaths.growth.smooth}else{deaths.growth}, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
   ggtitle("Daily deaths growth") +
-  xlab("Date (3-day average)") +
+  xlab(date.axis.smooth) +
   colorscale +
   theme1
 
@@ -207,28 +243,14 @@ G4 = df.dgrowth %>%
 ################################################
 # DAILY GROWTH RATE
 
-y.lim.rate = coord_cartesian(ylim = c(0,0.8))
-y.ticks.pc = scale_y_continuous(labels = scales::percent)
-
 # Graph #5: daily case growth rate, with 3-day smoothing
-df.growth.rate = df %>%
-  mutate(cases.growth.rate = (cases-lag(cases))/lag(cases)) %>%
+G5 = df.trends %>%
   filter(cases >= CASEFILTER) %>%
-  filter(!is.na(cases.growth.rate) & cases.growth.rate >= 0) %>%
-  mutate(cases.growth.rate.smooth = zoo::rollapply(
-    cases.growth.rate, mean, width = 3, by = 1, align = "center", fill = NA
-  )) %>%
-  group_by(county) %>%
-  filter(date > min(date) & date < max(date)) %>%
-  ungroup() %>%
-  mutate(doublingtime = log(2)/log(1+cases.growth.rate.smooth))
-G5 = df.growth.rate %>%
-  filter(county != "Unknown") %>%
-  ggplot(aes(x = date, y = cases.growth.rate.smooth, color = county)) +
+  ggplot(aes(x = date, y = if(SMOOTH){cases.growth.rate.smooth}else{cases.growth.rate}, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
-  ggtitle(paste0("Daily cases growth rate", " (N > ", CASEFILTER, ")")) +
-  xlab("Date (3-day average)") +
+  ggtitle(paste0("Daily cases growth rate", " (N > ", CASEFILTER, ", log-scale)")) +
+  xlab(date.axis.smooth) +
   y.lim.rate +
   y.ticks.pc +
   MApolicy1 + MApolicy2 +
@@ -236,24 +258,13 @@ G5 = df.growth.rate %>%
   theme1
 
 # Graph #6: daily death growth rate, with 3-day smoothing
-df.dgrowth.rate = df %>%
-  mutate(deaths.growth.rate = (deaths-lag(deaths))/lag(deaths)) %>%
+G6 = df.trends %>%
   filter(deaths >= DEATHFILTER) %>%
-  filter(!is.na(deaths.growth.rate) & deaths.growth.rate >= 0) %>%
-  mutate(deaths.growth.rate.smooth = zoo::rollapply(
-    deaths.growth.rate, mean, width = 3, by = 1, align = "center", fill = NA
-  )) %>%
-  group_by(county) %>%
-  filter(date > min(date) & date < max(date)) %>%
-  ungroup() %>%
-  mutate(ddoublingtime = log(2)/log(1+deaths.growth.rate.smooth))
-G6 = df.dgrowth.rate %>%
-  filter(county != "Unknown") %>%
-  ggplot(aes(x = date, y = deaths.growth.rate.smooth, color = county)) +
+  ggplot(aes(x = date, y = if(SMOOTH){deaths.growth.rate.smooth}else{deaths.growth.rate}, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
-  ggtitle(paste0("Daily deaths growth rate", " (N > ", DEATHFILTER, ")")) +
-  xlab("Date (3-day average)") +
+  ggtitle(paste0("Daily deaths growth rate", " (N > ", DEATHFILTER, ", log-scale)")) +
+  xlab(date.axis.smooth) +
   y.lim.rate +
   y.ticks.pc +
   colorscale +
@@ -264,17 +275,14 @@ G6 = df.dgrowth.rate %>%
 #############################
 # DOUBLING TIME
 
-y.ticks.weeks = scale_y_continuous(breaks = c(0,7,14,21,28,35,42,49,56))
-y.lim.wks = coord_cartesian(ylim = c(0, 7*DOUBLINGWKS))
-
 # Graph #7: cases doubling time, with 3-day smoothing
-G7 = df.growth.rate %>%
-  filter(county != "Unknown") %>%
+G7 = df.trends %>%
+  filter(cases >= CASEFILTER) %>%
   ggplot(aes(x = date, y = doublingtime, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
   ggtitle(paste0("Confirmed cases doubling time (days)", " (N > ", CASEFILTER, ")")) +
-  xlab("Date (3-day average)") +
+  xlab(date.axis.smooth) +
   y.lim.wks +
   y.ticks.weeks +
   MApolicy1 + MApolicy2 +
@@ -282,13 +290,13 @@ G7 = df.growth.rate %>%
   theme1
 
 # Graph #8: deaths doubling time, with 3-day smoothing
-G8 = df.dgrowth.rate %>%
-  filter(county != "Unknown") %>%
+G8 = df.trends %>%
+  filter(deaths >= DEATHFILTER) %>%
   ggplot(aes(x = date, y = ddoublingtime, color = county)) +
   geom_line_plot +
   x.ticks.halfm +
   ggtitle(paste0("Confirmed deaths doubling time (days)", " (N > ", DEATHFILTER, ")")) +
-  xlab("Date (3-day average)") +
+  xlab(date.axis.smooth) +
   y.lim.wks +
   y.ticks.weeks +
   colorscale +
@@ -299,7 +307,12 @@ G8 = df.dgrowth.rate %>%
 #####################
 
 # Plot time!
-# grid.arrange(G1,G3,G2,G4,G5,G6,G7,G8, nrow = 4, top = textGrob(STATE, gp = gpar(fontface = "bold", fontsize = 24)))  # for viewing
-G = arrangeGrob(G1,G3,G2,G4,G5,G6,G7,G8, nrow = 4, top = textGrob(STATE, gp = gpar(fontface = "bold", fontsize = 24)))
-ggsave(file = paste0("covid19_", STATE, ".pdf"), G, width = 12, height = 12, units = "in")
-ggsave(file = paste0("covid19_", STATE, ".png"), G, width = 12, height = 12, units = "in", dpi = 320)
+titleobj = textGrob(paste0(STATE, " (up to ", maxDate, ")"), gp = gpar(fontface = "bold", fontsize = 24))
+
+if(!SAVEPLOTS){
+  grid.arrange(G1,G3,G2,G4,G5,G6,G7,G8, nrow = 4, top = titleobj)  # for viewing
+}else{
+  G = arrangeGrob(G1,G3,G2,G4,G5,G6,G7,G8, nrow = 4, top = titleobj)
+  ggsave(file = paste0("covid19_", STATE, ".pdf"), G, width = 12, height = 12, units = "in")
+  ggsave(file = paste0("covid19_", STATE, ".png"), G, width = 12, height = 12, units = "in", dpi = 320)
+}
